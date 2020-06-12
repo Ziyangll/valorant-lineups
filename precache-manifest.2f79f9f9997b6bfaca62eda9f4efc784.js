b8fc406a391437c9b64d0d7f234cae372226a429
Ziyang Li <ZL@auburn.edu>
2020-06-12 16:13:06 -0500

    Updates

---
+++ precache-manifest.2f79f9f9997b6bfaca62eda9f4efc784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec894c51d034b18f31e0c5aacd5895b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4bed1b4f4b550aa11a2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d4eaed663eb936018b1",
+    "url": "/static/js/2.f0a2d3b6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f0a2d3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4bed1b4f4b550aa11a2",
+    "url": "/static/js/main.747279d1.chunk.js"
+  },
+  {
+    "revision": "60ed873ed558eccbeb7a",
+    "url": "/static/js/runtime-main.09861085.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/valorant-lineups/precache-manifest.fb8e19a19bd462788ad660556e2b2967.js"
+  "/precache-manifest.2f79f9f9997b6bfaca62eda9f4efc784.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/valorant-lineups/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
