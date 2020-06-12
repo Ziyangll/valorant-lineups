b0ae76428d126606376e4e4df14626d13a87356d
Ziyang Li <ZL@auburn.edu>
2020-06-12 16:12:37 -0500

    dep

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { AppBar, Toolbar, Button, ThemeProvider } from "@material-ui/core";
-import Home from "./components/home/Home";
+import Home from "./components/Home";
 import Cypher from "./components/Cypher";
 
 export default function App() {
